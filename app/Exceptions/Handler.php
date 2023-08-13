2b3202f8530368608d5d3f8df77e4d93fea5f8ce
Ph.Hitachi <phhit@LAPTOP-U4TRD80E.com>
2023-08-13 17:07:20 +0800

    Update: Add Exceptions for API Error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,15 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Support\Exceptions\OAuthException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Support\Exceptions\GraphApiException;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -23,8 +30,20 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        $this->reportable(
+            reportUsing: fn(TokenInvalidException | TokenExpiredException | TokenBlacklistedException $e) => throw new OAuthException(code: 'token_could_not_verified')
+        );
+
+        $this->reportable(
+            reportUsing: fn(JWTException $e) => throw new OAuthException(code: 'token_could_not_parse', statusCode: Response::HTTP_INTERNAL_SERVER_ERROR)
+        );
+
+        $this->reportable(
+            reportUsing: fn(Throwable $e) => //dd($e)
+            throw new GraphApiException(
+                //check if app is in production
+                message: config('app.env') !== 'production' ? $e->getMessage() : 'Sorry, There was something went wrong on our side, Please try again later.'
+            )
+        );
     }
 }
