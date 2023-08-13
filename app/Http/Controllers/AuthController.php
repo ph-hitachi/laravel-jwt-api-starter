4d27cefeceae3c5f2a99e5bb706aa39f67eb1d22
Ph.Hitachi <phhit@LAPTOP-U4TRD80E.com>
2023-08-13 17:12:46 +0800

    Update: Create Controller & routes for API Endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Support\Exceptions\OAuthException;
+use App\Support\Traits\Authenticatable;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    use Authenticatable;
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request): JsonResponse
+    {
+        if (!$token = Auth::attempt(credentials: $request->credentials())) {
+            throw new OAuthException(code: 'invalid_credentials_provided');
+        }
+
+        return $this->responseWithToken(access_token: $token);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \App\Modules\Auth\Collections\TokenResource
+     */
+    public function refresh(): JsonResponse
+    {
+        return $this->responseWithToken(access_token: auth()->refresh());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout(): JsonResponse
+    {
+        auth()->logout();
+
+        return new JsonResponse(['sucess' => true]);
+    }
+}
---
+++ lang/en/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+     */
+
+    'password'                     => 'The provided password is incorrect.',
+    'throttle'                     => 'Too many login attempts. Please try again in :seconds seconds.',
+    'invalid_credentials_provided' => 'These credentials do not match our records.',
+    'token_could_not_verified'     => 'The provided token is invalid, has expired, or has been blacklisted.',
+    'token_could_not_parse'        => 'An error occurred while processing the token.',
+
+];
